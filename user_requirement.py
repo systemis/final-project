ea1354d30d7046bc6db170f6ce0f8ae43d23bad6
systemis <cuthinh200@icloud.com>
2021-09-29 11:22:08 +0700

    first commit

---
+++ user_requirement.py
@@ -0,0 +1,5 @@
+min_price = 20000
+max_price = 200000
+building_type = 'F' #  Type D = Detached, S = Semi-Detached, T = Terraced, F = Flats/Maisonettes, O = Other
+duration = 'F' #  F = Freehold, L= Leasehold
+ppd_type = 'A' # A = Standard Price Paid entry B = Additional Price Paid entry
